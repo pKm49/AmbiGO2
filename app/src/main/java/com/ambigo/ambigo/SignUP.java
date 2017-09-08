7151cccc778294b7d80e97e0ced5e524b013dc8b
Prashanth K M <prasanthkm49@gmail.com>
2017-09-09 01:49:24 +0530

    added name and mobile number fields to the signup page. Name and Mobile number will be added as the child for each DriverID

--- app/src/main/java/com/ambigo/ambigo/SignUP.java
+++ app/src/main/java/com/ambigo/ambigo/SignUP.java
@@ -17,17 +17,21 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.StreamDownloadTask;
 
 public class SignUP extends AppCompatActivity {
 
-    private EditText mEmail,mPassword;
+    private EditText mName,mMob,mEmail,mPassword;
     private Button mSignUp;
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthListener;
+
+    private UserInfo mUserInfo;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,18 +49,21 @@ public class SignUP extends AppCompatActivity {
                 }
             }
         };
-
+        mName = (EditText)findViewById(R.id.name);
         mEmail = (EditText)findViewById(R.id.email);
+        mMob = (EditText)findViewById(R.id.mob_num);
         mPassword = (EditText) findViewById(R.id.password);
+
 
         mSignUp = (Button) findViewById(R.id.sign_up);
 
         mSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final String name = mName.getText().toString();
                 final String email = mEmail.getText().toString();
+                final String mobile = mMob.getText().toString();
                 final String password = mPassword.getText().toString();
-
 
                 mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(SignUP.this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -65,16 +72,12 @@ public class SignUP extends AppCompatActivity {
                             Toast.makeText(SignUP.this, "SignUP Failed",Toast.LENGTH_LONG).show();
                         }else{
                             String user_id = mAuth.getCurrentUser().getUid();
-                            DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child("Drivers").child(user_id);
-                           current_user_db.setValue(true);
-                            /* mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(SignUP.this, new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if(!task.isSuccessful()){
-                                       Toast.makeText(SignUP.this, "Sign in Failed",Toast.LENGTH_LONG).show();
-                                    }
-                                }
-                            });*/
+                            DatabaseReference current_user_id_db = FirebaseDatabase.getInstance().getReference().child("Users").child("Drivers").child(user_id);
+                            current_user_id_db.setValue(true);
+                            DatabaseReference current_user_name_db = FirebaseDatabase.getInstance().getReference().child("Users").child("Drivers").child("Name");
+                            current_user_name_db.setValue(name);
+                            DatabaseReference current_user_mob_db = FirebaseDatabase.getInstance().getReference().child("Users").child("Drivers").child("Phone_No");
+                            current_user_mob_db.setValue(mobile);
                         }
                     }
                 });
